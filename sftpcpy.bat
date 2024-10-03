85ac68ffc0bd7281f690466173e7898797d298c0
HARI PRASATH V S <126314387+Harihacks2git@users.noreply.github.com>
2024-10-03 11:40:42 +0530

    Update sftpcpy.bat
    Updated

--- sftpcpy.bat
+++ sftpcpy.bat
@@ -21,18 +21,24 @@ for /l %%i in (2022103031, 1, 2022103044) do (
         continue
     )
 
-    :: Perform SFTP operations for the current ID
     echo Trying s%%i@%ip% ...
     
-    :: SFTP command (this assumes you have sftp installed and available in PATH)
-    sftp s%%i@%ip% >> sftp_script.txt
-    echo mkdir cp_try >> sftp_script.txt
+    :: Create the SFTP batch command file
+    echo mkdir cp_try > sftp_script.txt
     echo put -r wk09 cw9 >> sftp_script.txt
     echo bye >> sftp_script.txt
-    sftp -b sftp_script.txt s%%i@%ip%
-    del sftp_script.txt
 
-    echo Copied to %%i successfully!
+    :: Execute the SFTP command with the batch file
+    sftp -b sftp_script.txt s%%i@%ip%
+
+    :: Check if SFTP succeeded
+    if %errorlevel% equ 0 (
+        echo Copied to %%i successfully!
+    ) else (
+        echo Failed to copy to %%i.
+    )
+
+    del sftp_script.txt
 )
 
 endlocal
